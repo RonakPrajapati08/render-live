770c303b7cf59957d8f3d3a3cab4d66d6bd57d7f
ronakprajapati08 <ronakprajapati3545@gmail.com>
2025-10-24 21:52:43 +0530

    New Node_modules add

--- server/server.js
+++ server/server.js
@@ -47,4 +47,4 @@ app.post("/send-notification", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
-});
+}); 
