26dac805d0d00376c0d07bbbd1c6cc26b8182612
ronakprajapati08 <ronakprajapati3545@gmail.com>
2025-11-01 10:32:09 +0530

    Build and update site

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -46,7 +46,7 @@ const messaging = getMessaging(app);
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/firebase-messaging-sw.js")
+    .register("/render-live/firebase-messaging-sw.js")
     .then((registration) => console.log("SW registered", registration))
     .catch((err) => console.error("SW registration failed", err));
 }
